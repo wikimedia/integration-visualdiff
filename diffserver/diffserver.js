f9c32d8bcf72358e877301045dce79e8e049a830
Subramanya Sastry <ssastry@wikimedia.org>
2016-02-10 21:40:52 -0600

    Clean up diffserver code
    * Use express 4.x + update package.json.
    * Generate a new diff only if one of the 3 images is missing.
    * General style cleanup.
    
    Change-Id: Icfb309616b009f4733a65596e0e2135d01a694bb

--- diffserver/diffserver.js
+++ diffserver/diffserver.js
@@ -1,109 +1,118 @@
 #!/usr/bin/env node
-( function () {
-"use strict";
 
-var express = require( 'express' ),
-	yargs = require( 'yargs' ),
-	fs = require('fs'),
-	Util = require('../lib/differ.utils.js').Util,
-	Differ = require('../lib/differ.js').VisualDiffer;
+'use strict';
 
-var defaults = {};
+var express = require('express');
+var yargs = require('yargs');
+var fs = require('fs');
+var Util = require('../lib/differ.utils.js').Util;
+var VisualDiffer = require('../lib/differ.js').VisualDiffer;
 
 // Command line options
-var opts = yargs.usage( 'Usage: $0 [connection parameters]' )
-	.options( 'help', {
+var opts = yargs.usage('Usage: $0 [connection parameters]')
+	.options('help', {
 		'boolean': true,
 		'default': false,
-		describe: "Show usage information."
-	} )
-	.options( 'P', {
+		describe: 'Show usage information.'
+	})
+	.options('config', {
+		describe: 'Configuration file for the server',
+		'default': './diffserver.settings.js',
+	})
+	.options('P', {
 		alias: 'port',
 		'default': 8002,
 		describe: 'Port number to use for connection.'
-	} );
+	});
 
 var argv = opts.argv;
 
-if ( argv.help ) {
+if (argv.help) {
 	opts.showHelp();
-	process.exit( 0 );
+	process.exit(0);
 }
 
 // Settings file
 var settings;
 try {
-	settings = require('./diffserver.settings.js');
-} catch ( e ) {
-	console.error("Aborting! Got exception processing diffserver.settings.js: " + e);
+	settings = require(argv.config);
+} catch (e) {
+	console.error('Aborting! Got exception processing diffserver.settings.js: ' + e);
 	console.error(e.stack);
 	return;
 }
 
-var baseDir = settings.outdir.slice().replace("\/$", "");
+var baseDir = settings.outdir.slice().replace('\/$', '');
 
 // Make an app
-var app = express.createServer();
+var app = express();
 
 // Declare static directory
-app.use("/pngs", express.static(__dirname + "/pngs"));
-
-// Add in the bodyParser middleware (because it's pretty standard)
-app.use( express.bodyParser() );
+app.use('/images', express.static(baseDir));
 
 // robots.txt: no indexing.
-app.get(/^\/robots.txt$/, function ( req, res ) {
-	res.end( "User-agent: *\nDisallow: /\n" );
+app.get(/^\/robots.txt$/, function (req, res) {
+	res.end('User-agent: *\nDisallow: /\n');
 });
 
+function sendResponse(res, opts) {
+	var pageTitle = 'Visual diff for ' + opts.wiki + ':' + opts.title;
+	var page = '<html>';
+	page += '<head><title>' + pageTitle + '</title></head>';
+	page += '<body>';
+	page += '<h1>' + pageTitle + '</h1>';
+	page += '<ul>';
+	// Set up relative links.
+	// -- walk 2 levels up (/diff/wikiprefix/) to set up the right urls.
+	page += '<li><a href="../../' + opts.html1.screenShot.replace(baseDir, 'images/') + '">' + opts.html1.name + ' Screenshot</a></li>';
+	page += '<li><a href="../../' + opts.html2.screenShot.replace(baseDir, 'images/') + '">' + opts.html2.name + ' Screenshot</a></li>';
+	page += '<li><a href="../../' + opts.diffFile.replace(baseDir, 'images/') + '">Visual Diff</a></li>';
+	page += '</ul></body>';
+	page += '</html>';
+
+	// Send response
+	res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+	res.status(200).send(page);
+}
+
 app.get(/^\/diff\/([^/]*)\/(.*)/, function(req, res) {
-	var wiki = req.params[0],
-		title = req.params[1],
-		oldId = req.query.oldId,
-		logger = settings.quiet ? function(){} : function(msg) { console.log(msg); };
+	var wiki = req.params[0];
+	var title = req.params[1];
+	var oldId = req.query.oldId;
+	var logger = settings.quiet ? function(){} : function(msg) { console.log(msg); };
 
 	// Clone before modifying it!
 	var opts = Util.clone(settings);
 	opts.wiki = wiki;
 	opts.title = title;
 	opts = Util.getNonCLIOpts(opts);
-	Differ.genVisualDiff(opts, logger,
-		function(err, diffData) {
-			if (err) {
-				console.error("ERROR for " + wiki + ':' + title + ': ' + err);
-				res.send("Encountered error [" + err + "] for " + wiki + ":" + title, 500);
-				return;
+
+	if (fs.existsSync(opts.diffFile) &&
+		fs.existsSync(opts.html1.screenShot) &&
+		fs.existsSync(opts.html1.screenShot)) {
+		// Everything found on disk .. send them along!
+		sendResponse(res, opts);
+	} else {
+		VisualDiffer.genVisualDiff(opts, logger,
+			function(err, diffData) {
+				if (err) {
+					console.error('ERROR for ' + wiki + ':' + title + ': ' + err);
+					res.send('Encountered error [' + err + '] for ' + wiki + ':' + title, 500);
+					return;
+				}
+
+				// Dump diff
+				var png_data = diffData.getImageDataUrl('').replace(/^data:image\/png;base64,/, '');
+				var png_buffer = new Buffer(png_data, 'base64');
+				fs.writeFileSync(opts.diffFile, png_buffer);
+
+				// Send HTML
+				sendResponse(res, opts);
 			}
-
-			// Dump diff
-			var png_data = diffData.getImageDataUrl("").replace(/^data:image\/png;base64,/, '');
-			var png_buffer = new Buffer(png_data, 'base64');
-			fs.writeFileSync(opts.diffFile, png_buffer);
-
-			// HTML
-			var pageTitle = "Visual diff for " + wiki + ":" + title;
-			var page = "<html>";
-			page += "<head><title>" + pageTitle + "</title></head>";
-			page += "<body>";
-			page += "<h1>" + pageTitle + "</h1>";
-			page += "<ul>";
-			// Set up relative links.
-			// -- walk 2 levels up (/diff/wikiprefix/) to set up the right urls.
-			page += "<li><a href='../../" + opts.html1.screenShot.replace(baseDir, "pngs") + "'>" + opts.html1.name + " Screenshot</a></li>";
-			page += "<li><a href='../../" + opts.html2.screenShot.replace(baseDir, "pngs") + "'>" + opts.html2.name + " Screenshot</a></li>";
-			page += "<li><a href='../../" + opts.diffFile.replace(baseDir, "pngs") + "'>Visual Diff</a></li>";
-			page += "</ul></body>";
-			page += "</html>";
-
-			// Send response
-			res.setHeader( 'Content-Type', 'text/html; charset=UTF-8' );
-			res.send( page, 200 );
-		}
-	);
+		);
+	}
 });
 
 // Start the app
-app.listen( argv.port );
-console.log( "Listening on port: " + argv.port );
-
-}() );
+app.listen(argv.port);
+console.log('Listening on port: ' + argv.port);
