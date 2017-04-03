e5ce302e8ab51303d8d2fc49f6463815e9c3adee
Subramanya Sastry <ssastry@wikimedia.org>
2017-04-03 15:01:41 -0500

    Provide the getTestRunId function required by parsoid-vd on ruthenium
    * Read from a file and return the first line as the commit id
    
    Change-Id: Id7d16e5a80ba899b0535d80b888fcc3deb52466d

--- testreduce/client.scripts.js
+++ testreduce/client.scripts.js
@@ -53,45 +53,13 @@ function generateVisualDiff(opts, test) {
 	});
 }
 
-function gitCommitFetch(opts) {
-	// Make a copy since we are going to modify it
-	opts = Util.clone(opts);
-	var parsoidServer = Util.getNonCLIOpts(opts).html2.server;
-	var requestOptions = {
-		uri: parsoidServer + '_version',
-		proxy: process.env.HTTP_PROXY_AND_PORT || '',
-		method: 'GET'
-	};
-
-	return new Promise(function(resolve, reject) {
-		retryingHTTPRequest(10, requestOptions, function(error, response, body) {
-			var err;
-			if (error || !response) {
-				err = 'Error could not find the current commit from ' + parsoidServer;
-				console.error(err);
-				reject(err);
-			} else if (response.statusCode === 200) {
-				try {
-					var resp = JSON.parse(body);
-					var lastCommit = resp.sha;
-					var lastCommitTime = (new Date()).toISOString();
-					resolve([lastCommit, lastCommitTime]);
-				} catch (e) {
-					err = 'Got response: ' + body + ' from ' + requestOptions.uri;
-					err = err + '\nError extracing commit SHA from it: ' + e;
-					console.error(err);
-					reject(err);
-				}
-			} else {
-				err = requestOptions.uri + ' responded with a HTTP status ' + response.statusCode;
-				console.error(err);
-				reject(err);
-			}
-		});
-	});
+// Read ids from a file and return the first line of the file
+var fs = require('fs');
+function getTestRunId(opts) {
+	return fs.readFileSync(opts.testRunIdFile || './testrun.ids', 'utf-8').split('\n')[0];
 }
 
 if (typeof module === 'object') {
-	module.exports.gitCommitFetch = gitCommitFetch;
+	module.exports.getTestRunId = getTestRunId;
 	module.exports.generateVisualDiff = generateVisualDiff;
 }
