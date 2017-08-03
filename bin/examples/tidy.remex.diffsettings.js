221d4e152201b7e62615ead7f1a48c297b217f71
Subramanya Sastry <ssastry@wikimedia.org>
2017-08-02 23:39:30 -0500

    Update Tidy-vs-Remex config to use a preload adaptor
    This adaptor:
    * loads Tidy HTML via the standard Mediawiki URL
    * fetches Remex body HTML via the parsermigration-api
    * splices out the Tidy body and splices in the Remex body
    * adds base hrefs to both the HTML strings and saves them to disk
    * updates config to have phantom read from the local files
    * after diffing, deletes the files (only on success)
    
    Change-Id: If2f733b2aca75e6123c4b2026400d5ad34f72e0b

--- bin/examples/tidy.remex.diffsettings.js
+++ bin/examples/tidy.remex.diffsettings.js
@@ -1,29 +1,34 @@
+var adaptor = require('../../lib/tidy_vs_remex.adaptor.js');
+
 module.exports = {
+  preLoadHandler: adaptor.pre,
+  postRenderHandler: adaptor.post,
+
   // Production wikipedia PHP parser output
   html1: {
     name: 'tidy',
-    postprocessorScript: '../lib/pm.old.postprocess.js',
+    postprocessorScript: '../lib/tidy_vs_remex.postprocess.js',
     injectJQuery: false,
     // suppress default base url computation code
     // since we are providing the full wiki domain
     // on the commandline
     server: 'https://',
     computeURL: function(server, domain, title) {
-      return server + domain + '/wiki/' + encodeURIComponent(title) + '?action=parsermigration-edit';
+      return server + domain + '/wiki/' + encodeURIComponent(title);
     },
 	// dumpHTML: true,
   },
 
   html2: {
     name: 'remex',
-    postprocessorScript: '../lib/pm.new.postprocess.js',
+    postprocessorScript: '../lib/tidy_vs_remex.postprocess.js',
     injectJQuery: false,
     // suppress default base url computation code
     // since we are providing the full wiki domain
     // on the commandline
     server: 'https://',
     computeURL: function(server, domain, title) {
-      return server + domain + '/wiki/' + encodeURIComponent(title) + '?action=parsermigration-edit';
+      return server + domain + '/wiki/' + encodeURIComponent(title);
     },
 	// dumpHTML: true,
   },
