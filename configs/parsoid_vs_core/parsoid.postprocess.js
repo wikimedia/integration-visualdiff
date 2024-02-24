af32bf04e786c9b8621292a6ba75f66d3766e3e2
Subramanya Sastry <ssastry@wikimedia.org>
2024-02-24 10:46:50 -0600

    Fix hiding of 'rendered by parsoid' indicator
    Looks like it had an id and it is better since it does a better
    of job of suppressing whitespace diffs by hiding the entire
    indicator.
    
    Change-Id: Ic0eea11efd1fc34e7aa515835e33c462e302efe9

--- configs/parsoid_vs_core/parsoid.postprocess.js
+++ configs/parsoid_vs_core/parsoid.postprocess.js
@@ -40,8 +40,7 @@ window.postprocessDOM = function(customCSS) {
 	// Hide notifications (Parsoid adds a new one now -- so all notifications)
 	$('div.mw-notification').hide();
 	// Hide Parsoid's "rendered with parsoid" indicator
-	// But this is too crude a hammer without an id on it.
-	$('div.cdx-info-chip').hide();
+	$('div#mw-indicator-parsoid').hide();
 
 	return null;
 };
