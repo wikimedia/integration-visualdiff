d3efc2c05d20483e06168135bcc913eb0154af6e
Subramanya Sastry <ssastry@wikimedia.org>
2026-01-16 17:00:52 -0500

    MFE: Delete Parsoid enwikivoyage-specific workarounds
    Change-Id: I6481b6b6ee872141198fc82d39567e880a77d183

--- configs/parsoid_vs_core_mfe/parsoid.postprocess.js
+++ configs/parsoid_vs_core_mfe/parsoid.postprocess.js
@@ -17,25 +17,6 @@ window.postprocessDOM = function(customCSS) {
 	// Hide footer -- adds imperceptible noise in some tests
 	$('footer').hide();
 
-	// Workaround for https://phabricator.wikimedia.org/T374883.
-	// Make collapsible h3s caused by GIGO etc.. behave like legacy parser.
-	const invalidHeadings = $( '.mw-heading.mf-collapsible-heading:not(.mw-heading2)' );
-	invalidHeadings.find( '.mf-icon' ).remove();
-	invalidHeadings
-		.css( 'padding-left', 0 )
-		.css( 'border-bottom', 0 )
-		.removeClass('mf-collapsible-heading' );
-
-	// enwikivoyage specific hacks:
-	// 1. Hide related pages (which seems to come from some gadget which
-	//    don't always reliably load before the screenshotting and lead to
-	//    a lot of false positives and hence noise)
-	// 2. Suppress gadget-added add listing links
-	$('div#mw-data-after-content').hide();
-	// For some reason, these don't seem to consistently get added everywhere during visual diff tests
-	// but they are present when visited on the wiki -- so, suppress the noise for now
-	$('a.listingeditor-add').hide();
-
 	// T355099
 	$('p > br').each((i, br) => {
 		if ( br.parentNode.children.length === 1 ) {
