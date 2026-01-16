1d48715b65cdd1a5a0034e534ddbaf8ad2c7d899
Subramanya Sastry <ssastry@wikimedia.org>
2026-01-16 17:30:46 -0500

    MFE: Don't hide maps + remove legacy enwikivoyage-specific hacks
    Change-Id: Ib40e1a351b61f40dc3934bc21b439bc7ac605e49

--- configs/parsoid_vs_core_mfe/legacy.postprocess.js
+++ configs/parsoid_vs_core_mfe/legacy.postprocess.js
@@ -2,24 +2,11 @@ window.postprocessDOM = function(customCSS) {
 	// Add custom CSS to reduce rendering diffs
 	$('<style type="text/css">' + customCSS + '</style>').appendTo('head');
 
-	// Hide all maps
-	$('.mw-kartographer-map').hide();
-
 	// Hide notifications
 	$('div.mw-notification-area').hide();
 
 	// Hide footer -- adds imperceptible noise in some tests
 	$('footer').hide();
-
-	// enwikivoyage specific hacks:
-	// 1. Hide related pages (which seems to come from some gadget which
-	//    don't always reliably load before the screenshotting and lead to
-	//    a lot of false positives and hence noise)
-	// 2. Suppress gadget-added add listing links
-	$('div#mw-data-after-content').hide();
-	// For some reason, these don't seem to consistently get added everywhere during visual diff tests
-	// but they are present when visited on the wiki -- so, suppress the noise for now
-	$('a.listingeditor-add').hide();
 
 	// T355099
 	$('p > br').each((i, br) => {
